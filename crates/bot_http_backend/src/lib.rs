1fc69b0790d3df13cb3527fdab2b10b7ffb1a241
artemushakov <hello@temq.dev>
2024-03-21 00:15:37 +0000

    adds HttpBackendBot struct

--- crates/bot_http_backend/src/lib.rs
+++ crates/bot_http_backend/src/lib.rs
@@ -1,1 +1,19 @@
+use api::{PageData, PageResult, PageWorker};
 
+pub struct HttpBotBackendParams {
+    backend_endpoint: String,
+}
+
+impl HttpBotBackendParams {
+    pub fn new(url: String) -> Self {
+        HttpBotBackendParams {
+            backend_endpoint: url,
+        }
+    }
+}
+
+impl PageWorker for HttpBotBackendParams {
+    async fn submit_page_generation(&self, page_data: PageData) -> anyhow::Result<PageResult> {
+        todo!()
+    }
+}
