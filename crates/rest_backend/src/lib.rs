ca487d14121f7c2dbb356762ab4fafaf377ab7b6
artemushakov <hello@temq.dev>
2024-03-22 00:00:30 +0000

    adds init function for the backend

--- crates/rest_backend/src/lib.rs
+++ crates/rest_backend/src/lib.rs
@@ -1,1 +1,21 @@
+use std::net::Ipv4Addr;
 
+use axum::routing::post;
+use axum::Router;
+use tokio::net::TcpListener;
+
+pub struct RestBackend {
+    port: u8,
+}
+
+pub async fn init(backend_config: RestBackend) -> anyhow::Result<()> {
+    let router = Router::new().route("v1/requestPageForUser", post(load_page));
+
+    let ipv4addr = Ipv4Addr::new(0, 0, 0, backend_config.port);
+    let listener = TcpListener::bind(ipv4addr.to_string()).await?;
+
+    axum::serve(listener, router).await?;
+    return Ok(());
+}
+
+async fn load_page() {}
