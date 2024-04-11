a011298af38209257cbeaddad0c77db83503b888
temq09 <hello@temq.dev>
2024-04-11 22:48:47 +0100

    Implements modes for the bot (#13)

---
+++ src/bot/bin/bot_args.rs
@@ -0,0 +1,23 @@
+use clap::{Parser, ValueEnum};
+
+#[derive(Parser)]
+#[command(about, long_about = None)]
+pub(crate) struct BotArgs {
+    /// Mode for the bot to run.
+    #[arg(long, value_enum, value_name = "MODE")]
+    pub(crate) mode: Option<Mode>,
+
+    /// Backend url the bot will interact with
+    #[arg(long, value_name = "URL")]
+    pub(crate) backend_url: Option<String>,
+}
+
+#[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, ValueEnum)]
+pub(crate) enum Mode {
+    /// Everything will work out of the box, no extra settings required.
+    /// Use it for simple installations
+    Standalone,
+    /// Thin bot mode that requires the separate backend for the tasks to be executed.
+    /// Use it to scale the backend workload
+    Distributed,
+}
--- src/bot/bin/main.rs
+++ src/bot/bin/main.rs
@@ -1,26 +1,36 @@
 use std::path::PathBuf;
 use std::sync::Arc;
 
+use anyhow::anyhow;
+use clap::Parser;
 use dptree::case;
 use teloxide::dispatching::UpdateHandler;
 use teloxide::types::InputFile;
 use teloxide::{prelude::*, utils::command::BotCommands};
 
 use api::{PageData, PageResult, PageWorker};
+use bot_http_backend::HttpBotBackendParams;
 use botbackend::parallel_page_worker::ParallelPageWorker;
 use proto::command::Command;
+
+use crate::bot_args::{BotArgs, Mode};
+
+mod bot_args;
 
 type HandlerResult = Result<(), Box<dyn std::error::Error + Send + Sync>>;
 
 #[tokio::main]
-async fn main() {
-    let worker = ParallelPageWorker::new("/Users/artemushakov/prog/tmp/singlefile".to_string());
+async fn main() -> anyhow::Result<()> {
     let bot = Bot::from_env();
+    let args = BotArgs::parse();
+    let worker = create_worker(args)?;
     Dispatcher::builder(bot, schema())
-        .dependencies(dptree::deps![Arc::new(worker)])
+        .dependencies(dptree::deps![worker])
         .build()
         .dispatch()
         .await;
+
+    Ok(())
 }
 
 fn schema() -> UpdateHandler<Box<dyn std::error::Error + Send + Sync + 'static>> {
@@ -34,7 +44,7 @@ async fn get_page(
     bot: Bot,
     url: String,
     message: Message,
-    worker: Arc<ParallelPageWorker>,
+    worker: Arc<dyn PageWorker>,
 ) -> HandlerResult {
     let page_data = PageData::from_url(url, message.chat.id.to_string());
     let page_result = worker.submit_page_generation(page_data).await?;
@@ -63,4 +73,21 @@ async fn print_help(bot: Bot, message: Message) -> HandlerResult {
         .await
         .unwrap();
     return Ok(());
+}
+
+fn create_worker(args: BotArgs) -> anyhow::Result<Arc<dyn PageWorker>> {
+    match args.mode.unwrap_or(Mode::Standalone) {
+        Mode::Standalone => {
+            let worker =
+                ParallelPageWorker::new("/Users/artemushakov/prog/tmp/singlefile".to_string());
+            Ok(Arc::new(worker))
+        }
+        Mode::Distributed => {
+            let backend_url = args
+                .backend_url
+                .ok_or_else(|| anyhow!("Backend urls must be set for the distributed variant"))?;
+            let worker = HttpBotBackendParams::new(backend_url);
+            Ok(Arc::new(worker))
+        }
+    }
 }
