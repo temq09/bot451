7d3cd0828f78272890c27326a393e81c5a8bc605
artemushakov <hello@temq.dev>
2024-04-13 23:15:41 +0100

    adds postgres cli args

--- src/backend/bin/backend_args.rs
+++ src/backend/bin/backend_args.rs
@@ -8,5 +8,18 @@ pub(crate) struct BackendArgs {
     pub(crate) postgres_url: Option<String>,
 
     /// Working directory where all pages will be downloaded initially
+    #[arg(long, value_name = "PATH")]
     pub(crate) work_dir: Option<String>,
+
+    /// PostgresSQL user name
+    #[arg(long, value_name = "USERNAME")]
+    pub(crate) pg_user: Option<String>,
+
+    /// PostgresSQL password
+    #[arg(long, value_name = "PASSWORD")]
+    pub(crate) pg_password: Option<String>,
+
+    /// PostgresSQL table
+    #[arg(long, value_name = "TABLE_NAME")]
+    pub(crate) pg_table: Option<String>,
 }
