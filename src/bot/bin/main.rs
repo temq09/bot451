ad3661b8e07c6074055d6c89e834a0435094aabb
artemushakov <hello@temq.dev>
2024-06-18 23:56:46 +0100

    removes the mode arg from the bot binary

--- src/bot/bin/main.rs
+++ src/bot/bin/main.rs
@@ -1,7 +1,7 @@
 use std::sync::Arc;
 use std::time::Duration;
 
-use anyhow::{anyhow, Context};
+use anyhow::Context;
 use clap::Parser;
 use dptree::case;
 use teloxide::dispatching::UpdateHandler;
@@ -9,7 +9,7 @@ use teloxide::{prelude::*, utils::command::BotCommands};
 
 use proto::command::Command;
 
-use crate::bot_args::{BotArgs, Mode};
+use crate::bot_args::BotArgs;
 use crate::bot_error::BotError;
 use crate::worker::page_loader::PageLoader;
 use crate::worker::remote_page_loader::RemotePageLoader;
@@ -92,27 +92,30 @@ async fn send_message(bot: Bot, chat_id: ChatId, message: String) -> HandlerResu
 }
 
 fn create_worker(args: BotArgs, bot: Bot) -> anyhow::Result<Box<dyn PageLoader>> {
-    match args.mode.unwrap_or(Mode::Standalone) {
-        Mode::Standalone => {
-            let singlefile_cli_path = args
-                .singlefile_cli
-                .context("SINGLEFILE_CLI env variable must be set for the standalone mode")?;
-
-            let work_dir = args
-                .work_dir
-                .context("Working dir path must be set for standalone mode")?;
-            Ok(Box::new(StandalonePageLoader::new(
-                singlefile_cli_path,
-                work_dir,
-                bot,
-            )))
-        }
-        Mode::Distributed => {
-            let backend_url = args
-                .backend_url
-                .ok_or_else(|| anyhow!("Backend urls must be set for the distributed variant"))?;
-            let loader = RemotePageLoader::new(backend_url.as_str())?;
-            Ok(Box::new(loader))
-        }
+    match args.backend_url {
+        None => start_standalone(args.singlefile_cli, args.work_dir, bot),
+        Some(url) => start_distributed(&url),
     }
+}
+
+fn start_standalone(
+    singlefile_cli: Option<String>,
+    work_dir: Option<String>,
+    bot: Bot,
+) -> anyhow::Result<Box<dyn PageLoader>> {
+    let singlefile_cli_path = singlefile_cli
+        .context("SINGLEFILE_CLI env variable must be set for the standalone mode")?;
+
+    let work_dir = work_dir.context("Working dir path must be set for standalone mode")?;
+
+    Ok(Box::new(StandalonePageLoader::new(
+        singlefile_cli_path,
+        work_dir,
+        bot,
+    )))
+}
+
+fn start_distributed(backend_url: &str) -> anyhow::Result<Box<dyn PageLoader>> {
+    let loader = RemotePageLoader::new(backend_url)?;
+    Ok(Box::new(loader))
 }
